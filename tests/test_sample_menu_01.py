3edc8253b4ab9ba751424188776204eb33567037
nakamori1024 <79179090+nakamori1024@users.noreply.github.com>
2024-06-09 11:03:06 +0900

    Test assertion

--- tests/test_sample_menu_01.py
+++ tests/test_sample_menu_01.py
@@ -9,4 +9,4 @@ def test_cancel(qgis_app: QgsApplication, qgis_iface: QgisInterface):
     sample_menu_01.show()
     assert sample_menu_01.isVisible() is True
     sample_menu_01.ui.pushButton_cancel.click()
-    assert sample_menu_01.isVisible() is False
+    assert sample_menu_01.isVisible() is True
